82545a7ea15c77c8cc5d8b4af685de5a8c36e1a5
Kikuchi Jun <kikuchi@bonnou.com>
2020-04-29 16:28:45 +0900

    クライアント実行関数を実装

--- src/Discovery.hs
+++ src/Discovery.hs
@@ -4,11 +4,16 @@ module Discovery where
 
 import           RIO
 import           RIO.Text
+import           Network.HTTP.Client            ( newManager )
+import           Network.HTTP.Client.TLS        ( tlsManagerSettings )
 import           Servant.API
 import           Servant.Client                 ( BaseUrl(..)
                                                 , Scheme(Https)
                                                 , ClientM
+                                                , ClientError
                                                 , client
+                                                , mkClientEnv
+                                                , runClientM
                                                 )
 import           Discovery.List                 ( List )
 
@@ -34,3 +39,8 @@ list :: Maybe Name -> Maybe Preferred -> ClientM List
 getRest :: Text -> Text -> ClientM Desc
 
 (list :<|> getRest) = client api
+
+run :: ClientM a -> IO (Either ClientError a)
+run client = do
+  manager <- newManager tlsManagerSettings
+  runClientM client (mkClientEnv manager baseUrl)
