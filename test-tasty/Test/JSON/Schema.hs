10a15fd6b9ccb4e0f913115dffc6c83f137d208c
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-04 22:25:50 +0900

    integer のテストを追加実装

--- test-tasty/Test/JSON/Schema.hs
+++ test-tasty/Test/JSON/Schema.hs
@@ -196,3 +196,66 @@ spec_Test_JSON_Schema = describe "parse" $ do
             , stringFormat    = Just Byte
             }
       it "Schema にエンコード" $ decode json `shouldBe` Just schema
+  describe "integer" $ do
+    describe "Multiples" $ do
+      let json   = [r|{ "type": "integer", "multipleOf": 10 }|]
+          schema = Schema
+            { schemaType        = Just (IntegerType integerType)
+            , schemaTitle       = Nothing
+            , schemaDescription = Nothing
+            , schemaExamples    = Nothing
+            , schemaComment     = Nothing
+            , schemaEnum        = Nothing
+            , schemaConst       = Nothing
+            }
+          integerType = Integer
+            { integerMultipleOf       = Just 10
+            , integerMinimum          = Nothing
+            , integerMaximum          = Nothing
+            , integerExclusiveMinimum = Nothing
+            , integerExclusiveMaximum = Nothing
+            , integerFormat           = Nothing
+            }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "Range" $ do
+      let
+        json
+          = [r|{ "type": "integer", "minimum": 0, "maximum": 100, "exclusiveMinimum": true, "exclusiveMaximum": true }|]
+        schema = Schema
+          { schemaType        = Just (IntegerType integerType)
+          , schemaTitle       = Nothing
+          , schemaDescription = Nothing
+          , schemaExamples    = Nothing
+          , schemaComment     = Nothing
+          , schemaEnum        = Nothing
+          , schemaConst       = Nothing
+          }
+        integerType = Integer
+          { integerMultipleOf       = Nothing
+          , integerMinimum          = Just 0
+          , integerMaximum          = Just 100
+          , integerExclusiveMinimum = Just True
+          , integerExclusiveMaximum = Just True
+          , integerFormat           = Nothing
+          }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "Format" $ do
+      let json   = [r|{ "type": "integer", "format": "int32" }|]
+          schema = Schema
+            { schemaType        = Just (IntegerType integerType)
+            , schemaTitle       = Nothing
+            , schemaDescription = Nothing
+            , schemaExamples    = Nothing
+            , schemaComment     = Nothing
+            , schemaEnum        = Nothing
+            , schemaConst       = Nothing
+            }
+          integerType = Integer
+            { integerMultipleOf       = Nothing
+            , integerMinimum          = Nothing
+            , integerMaximum          = Nothing
+            , integerExclusiveMinimum = Nothing
+            , integerExclusiveMaximum = Nothing
+            , integerFormat           = Just Int32
+            }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
