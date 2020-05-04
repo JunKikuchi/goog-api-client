4424308d174a8cd5c58d93a3c4b0461bea440e61
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-04 22:51:45 +0900

    object の additionalProperties テストを追加実装

--- test-tasty/Test/JSON/Schema.hs
+++ test-tasty/Test/JSON/Schema.hs
@@ -383,3 +383,135 @@ spec_Test_JSON_Schema = describe "parse" $ do
         , objectPatternProperties    = Nothing
         }
     it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "additionalProperties bool" $ do
+      let
+        json
+          = [r|{ "type": "object", "properties": { "number": { "type": "number" }, "street_name": { "type": "string" }, "street_type": { "type": "string", "enum": ["Street", "Avenue", "Boulevard"]} }, "additionalProperties": false }|]
+        schema = Schema
+          { schemaType        = Just (ObjectType objectType)
+          , schemaTitle       = Nothing
+          , schemaDescription = Nothing
+          , schemaExamples    = Nothing
+          , schemaComment     = Nothing
+          , schemaEnum        = Nothing
+          , schemaConst       = Nothing
+          }
+        propertiess = Map.fromList
+          [ ( "number"
+            , Schema
+              (Just
+                (NumberType
+                  (Number Nothing Nothing Nothing Nothing Nothing Nothing)
+                )
+              )
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+            )
+          , ( "street_name"
+            , Schema
+              (Just (StringType (String Nothing Nothing Nothing Nothing)))
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+            )
+          , ( "street_type"
+            , Schema
+              (Just (StringType (String Nothing Nothing Nothing Nothing)))
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              (Just ["Street", "Avenue", "Boulevard"])
+              Nothing
+            )
+          ]
+        objectType = Object
+          { objectProperties           = Just propertiess
+          , objectAdditionalProperties = Just (AdditionalPropertiesBool False)
+          , objectRequired             = Nothing
+          , objectPropertyNames        = Nothing
+          , objectMinProperties        = Nothing
+          , objectMaxProperties        = Nothing
+          , objectDependencies         = Nothing
+          , objectPatternProperties    = Nothing
+          }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "additionalProperties schema" $ do
+      let
+        json
+          = [r|{ "type": "object", "properties": { "number": { "type": "number" }, "street_name": { "type": "string" }, "street_type": { "type": "string", "enum": ["Street", "Avenue", "Boulevard"]} }, "additionalProperties":  { "type": "string" } }|]
+        schema = Schema
+          { schemaType        = Just (ObjectType objectType)
+          , schemaTitle       = Nothing
+          , schemaDescription = Nothing
+          , schemaExamples    = Nothing
+          , schemaComment     = Nothing
+          , schemaEnum        = Nothing
+          , schemaConst       = Nothing
+          }
+        propertiess = Map.fromList
+          [ ( "number"
+            , Schema
+              (Just
+                (NumberType
+                  (Number Nothing Nothing Nothing Nothing Nothing Nothing)
+                )
+              )
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+            )
+          , ( "street_name"
+            , Schema
+              (Just (StringType (String Nothing Nothing Nothing Nothing)))
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+            )
+          , ( "street_type"
+            , Schema
+              (Just (StringType (String Nothing Nothing Nothing Nothing)))
+              Nothing
+              Nothing
+              Nothing
+              Nothing
+              (Just ["Street", "Avenue", "Boulevard"])
+              Nothing
+            )
+          ]
+        objectType = Object
+          { objectProperties           = Just propertiess
+          , objectAdditionalProperties = Just
+            (AdditionalPropertiesSchema
+              (Schema
+                (Just (StringType (String Nothing Nothing Nothing Nothing)))
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+              )
+            )
+          , objectRequired             = Nothing
+          , objectPropertyNames        = Nothing
+          , objectMinProperties        = Nothing
+          , objectMaxProperties        = Nothing
+          , objectDependencies         = Nothing
+          , objectPatternProperties    = Nothing
+          }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+
