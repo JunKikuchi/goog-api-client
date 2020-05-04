063ee55dc3ac9c595ac3eb1fb31c656044087c8b
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-04 22:30:00 +0900

    number のテストを追加実装

--- test-tasty/Test/JSON/Schema.hs
+++ test-tasty/Test/JSON/Schema.hs
@@ -259,3 +259,66 @@ spec_Test_JSON_Schema = describe "parse" $ do
             , integerFormat           = Just Int32
             }
       it "Schema にエンコード" $ decode json `shouldBe` Just schema
+  describe "number" $ do
+    describe "Multiples" $ do
+      let json   = [r|{ "type": "number", "multipleOf": 10 }|]
+          schema = Schema
+            { schemaType        = Just (NumberType numberType)
+            , schemaTitle       = Nothing
+            , schemaDescription = Nothing
+            , schemaExamples    = Nothing
+            , schemaComment     = Nothing
+            , schemaEnum        = Nothing
+            , schemaConst       = Nothing
+            }
+          numberType = Number
+            { numberMultipleOf       = Just 10
+            , numberMinimum          = Nothing
+            , numberMaximum          = Nothing
+            , numberExclusiveMinimum = Nothing
+            , numberExclusiveMaximum = Nothing
+            , numberFormat           = Nothing
+            }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "Range" $ do
+      let
+        json
+          = [r|{ "type": "number", "minimum": 0, "maximum": 100, "exclusiveMinimum": true, "exclusiveMaximum": true }|]
+        schema = Schema
+          { schemaType        = Just (NumberType numberType)
+          , schemaTitle       = Nothing
+          , schemaDescription = Nothing
+          , schemaExamples    = Nothing
+          , schemaComment     = Nothing
+          , schemaEnum        = Nothing
+          , schemaConst       = Nothing
+          }
+        numberType = Number
+          { numberMultipleOf       = Nothing
+          , numberMinimum          = Just 0
+          , numberMaximum          = Just 100
+          , numberExclusiveMinimum = Just True
+          , numberExclusiveMaximum = Just True
+          , numberFormat           = Nothing
+          }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
+    describe "Format" $ do
+      let json   = [r|{ "type": "number", "format": "double" }|]
+          schema = Schema
+            { schemaType        = Just (NumberType numberType)
+            , schemaTitle       = Nothing
+            , schemaDescription = Nothing
+            , schemaExamples    = Nothing
+            , schemaComment     = Nothing
+            , schemaEnum        = Nothing
+            , schemaConst       = Nothing
+            }
+          numberType = Number
+            { numberMultipleOf       = Nothing
+            , numberMinimum          = Nothing
+            , numberMaximum          = Nothing
+            , numberExclusiveMinimum = Nothing
+            , numberExclusiveMaximum = Nothing
+            , numberFormat           = Just Double
+            }
+      it "Schema にエンコード" $ decode json `shouldBe` Just schema
