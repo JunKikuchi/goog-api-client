3ee1723199ab183a2ff6747541f4337f7664f3a9
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-10 11:09:42 +0900

    setCurrentDirectory を withCurrentDirectory に変更

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -9,8 +9,7 @@ import qualified CodeGen.Project               as Proj
 gen :: Dist -> RestDescription -> IO ()
 gen dist desc = do
   Dir.createDirectoryIfMissing True dist
-  Dir.setCurrentDirectory dist
-
-  projectName <- Proj.projectName desc
-  Proj.clean projectName
-  Proj.gen projectName desc
+  Dir.withCurrentDirectory dist $ do
+    projectName <- Proj.projectName desc
+    Proj.clean projectName
+    Proj.gen projectName desc
