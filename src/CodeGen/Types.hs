5c967215aee3dfa1b18cb5d2d159a61f6aca61af
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-11 23:05:47 +0900

    コード整理

--- src/CodeGen/Types.hs
+++ src/CodeGen/Types.hs
@@ -4,18 +4,20 @@ import           RIO
 import qualified JSON.Schema                   as JSON
 import           RIO.Writer                     ( WriterT )
 
-type Dist = FilePath
-
-type ProjectName = Text
-type ProjectDir  = FilePath
-
-type SrcDir = FilePath
-
+type Dist       = FilePath
+type ProjectDir = FilePath
+type SrcDir     = FilePath
 type ServiceDir = FilePath
-type ServiceName = Text
-type ServiceVersion = Text
+type SchemaDir  = FilePath
 
-type SchemaDir = FilePath
+type ProjectName    = Text
+type ServiceName    = Text
+type ServiceVersion = Text
+type RecordName     = Text
+
+type Imports = Text
+type Record  = Text
+type Field   = Text
 
 type GenRecord  = WriterT [Object] IO
 type Object     = (ObjectName, JSON.Object)
@@ -23,8 +25,3 @@ type ObjectName = Text
 
 type GenRef = WriterT [Ref] IO
 type Ref    = Text
-
-type RecordName  = Text
-type Record      = Text
-type Field       = Text
-type Imports     = Text
