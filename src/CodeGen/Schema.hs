e45947ae5c02db8d2358af4156d6d3dbb409da96
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-11 21:27:54 +0900

    Schema ファイルの生成を実装

--- src/CodeGen/Schema.hs
+++ src/CodeGen/Schema.hs
@@ -5,25 +5,35 @@ import           Prelude                        ( print )
 import qualified RIO.Directory                 as Dir
 
 import           RIO
+import qualified RIO.ByteString                as B
+import qualified RIO.FilePath                  as FP
 import qualified RIO.Text                      as T
 import           Discovery.RestDescription
 import           Discovery.RestDescription.Schema
 import           CodeGen.Types
-import           CodeGen.Util                  ( get
+import           CodeGen.Util                   ( get
                                                 , withDir
                                                 )
 
+schemaName :: Text
+schemaName = "Schema"
+
 schemaDir :: SchemaDir
-schemaDir = T.unpack "Schema"
+schemaDir = T.unpack schemaName
 
 gen :: ServiceName -> ServiceVersion -> RestDescriptionSchemas -> IO ()
-gen name ver schemas = withDir schemaDir $ do
+gen svcName svcVer schemas = withDir schemaDir $ do
   dir <- Dir.getCurrentDirectory
   print dir
 
-  mapM_ (genSchema name ver) schemas
+  mapM_ (genFile svcName svcVer) schemas
 
-genSchema :: ServiceName -> ServiceVersion -> Schema -> IO ()
-genSchema name ver desc = do
-  scmId <- get schemaId "schemaId" desc
-  print [name, ver, scmId]
+genFile :: ServiceName -> ServiceVersion -> Schema -> IO ()
+genFile svcName svcVer desc = do
+  name <- get schemaId "schemaId" desc
+  let moduleName = T.intercalate "." [svcName, svcVer, schemaName, name]
+  print moduleName
+
+  let path    = FP.addExtension (T.unpack name) "hs"
+  let content = "module " <> moduleName <> " where"
+  B.writeFile path (T.encodeUtf8 content)
