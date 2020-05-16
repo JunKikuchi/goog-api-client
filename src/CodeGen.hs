8aa328b0434461bb91b812e7f19fc2cbc4628bc2
Kikuchi Jun <kikuchi@bonnou.com>
2020-05-17 01:08:12 +0900

    コードフォーマット

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -5,7 +5,7 @@ import           RIO
 import           Discovery.RestDescription      ( RestDescription )
 import qualified Discovery.RestDescription     as Desc
 import           CodeGen.Types
-import           CodeGen.Util                  ( get
+import           CodeGen.Util                   ( get
                                                 , toTitle
                                                 , withDir
                                                 )
